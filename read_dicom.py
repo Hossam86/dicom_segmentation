7811ff9595a6ab1c0eb8ddcfdc974abb95bb8773
Hossam Samir <hossamsamir@outlook.com>
2021-03-01 20:03:39 +0200

    pixel intensity values to housefield

--- read_dicom.py
+++ read_dicom.py
@@ -34,5 +34,27 @@ def load_dicom(folder_path):
             slices[0].SliceLocation-slices[1].SliceLocation)
     for s in slices:
         s.SliceThickness = slice_thickness
-        print(slice_thickness)
     return slices
+
+def get_pixels_hu(scans):
+    image=np.stack([s.pixel_array for s in scans])
+    image=image.astype(np.int16)
+
+    #set outside-of-scan pixels to 0
+    #The intercept is usually -1024, so air is approximatly 0
+    image[image==-2000]=0
+
+    # Convert to Hounsfield units (HU)
+    intercept = scans[0].RescaleIntercept
+    slope = scans[0].RescaleSlope
+    
+    if slope != 1:
+        image = slope * image.astype(np.float64)
+        image = image.astype(np.int16)
+        
+    image += np.int16(intercept)
+    
+    return np.array(image, dtype=np.int16)
+
+scans=load_dicom(folder_path)
+get_pixels_hu(scans)
